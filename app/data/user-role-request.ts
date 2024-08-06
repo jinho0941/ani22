1e3ec5d296a8c3cae6ebefff3e08e45d8d74dd70
daggax <daggax8235@gmail.com>
2024-08-07 04:09:04 +0900

    feat: 현재 유저 role request가져오는 함수 필요해서 추가.

--- app/data/user-role-request.ts
+++ app/data/user-role-request.ts
@@ -2,6 +2,8 @@
 
 import { db } from '@/lib/db'
 import { RoleRequestWithUser, WithCursor } from '@/type'
+import { UserRoleRequest } from '@prisma/client'
+import { getCurrentUserId } from '@/app/data/user'
 
 export const getUserRoleRequests = async (
   cursor?: string,
@@ -30,3 +32,23 @@ export const getUserRoleRequests = async (
     throw new Error('유저 권한을 가져오는 중 에러가 발생하였습니다.')
   }
 }
+
+export const getCurrentUserRoleRequest =
+  async (): Promise<UserRoleRequest | null> => {
+    try {
+      const userId = await getCurrentUserId()
+
+      const userRoleRequest = await db.userRoleRequest.findUnique({
+        where: {
+          userId,
+        },
+      })
+      if (!userRoleRequest) return null
+
+      return userRoleRequest
+    } catch (error) {
+      throw new Error(
+        '유저 권한 요청 데이터를 가져오는 중에 에러가 발생하였습니다.',
+      )
+    }
+  }
