d470767e1352b6d4d038e6310db54c8d76474060
daggax <daggax8235@gmail.com>
2024-08-06 20:49:52 +0900

    fix: logo 일반 택스트 모드와 링크 모드 두가지 받을수있게 변경

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,22 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
-export const Logo = () => {
+type Props = {
+  isLink?: boolean
+}
+
+export const Logo = ({ isLink = false }: Props) => {
+  if (!isLink)
+    return (
+      <div className='text-2xl border-b pb-1 px-1 border-black dark:border-white'>
+        Ani22
+      </div>
+    )
+
   return (
     <Button variant={'ghost'} className='p-1' asChild>
       <Link href={'/'}>
-        <div className='text-4xl border-b pb-2 px-1 border-black dark:border-white'>
+        <div className='text-2xl border-b pb-1 px-1 border-black dark:border-white'>
           Ani22
         </div>
       </Link>
