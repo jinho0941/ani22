07e037e107be0309a8eb91090f8cff45bddbcf19
daggax <daggax8235@gmail.com>
2024-08-06 19:52:47 +0900

    feat: middleware 초기 새팅 완료.

---
+++ middleware.ts
@@ -0,0 +1,49 @@
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  apiUploadThingPrefix,
+  authRoutes,
+  publicRoutes,
+} from '@/routes'
+import { auth } from './auth'
+import { NextResponse } from 'next/server'
+
+export default auth(async (req) => {
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(`/${apiAuthPrefix}`)
+  const isApiUploadthingRoute = nextUrl.pathname.startsWith(
+    `/${apiUploadThingPrefix}`,
+  )
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  const isFirst = req.cookies.has('isFirstLogin')
+  if (nextUrl.pathname !== '/topic') {
+    if (isLoggedIn && isFirst) {
+      return NextResponse.redirect(new URL('/topic', nextUrl))
+    }
+  }
+
+  if (isApiAuthRoute) {
+    return
+  }
+  if (isApiUploadthingRoute) {
+    return
+  }
+
+  if (isAuthRoute && isLoggedIn) {
+    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return NextResponse.redirect(new URL('/sign-in', nextUrl))
+  }
+
+  return NextResponse.next()
+})
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
