a3893b72cc04ca449410c9f049f0459c3e12d766
daggax <daggax8235@gmail.com>
2024-08-06 19:18:32 +0900

    feat: 랜덤 비디오 샘플 데이터 리스트 추가.

---
+++ constants.ts
@@ -0,0 +1,17 @@
+export const videoUrls = [
+  'https://utfs.io/f/0ad9219e-82cc-44ac-9702-7566fc192f5c-i4dsge.mp4',
+  'https://utfs.io/f/8939c264-e266-4950-90c3-a43c16a1b99e-4dt52s.mp4',
+  'https://utfs.io/f/43d1f36d-78bb-407a-9ed2-22818f7c1f5b-90g5e2.mp4',
+  'https://utfs.io/f/226cdcee-9615-4565-a6b9-303295c850aa-8x0g4h.mp4',
+  'https://utfs.io/f/10432523-4d58-4e9c-8ab8-1f6972b29217-cliw8f.mp4',
+  'https://utfs.io/f/47743e01-5af9-4f52-8618-3c440243c795-k2smc1.mp4',
+  'https://utfs.io/f/de67e81d-346f-48df-b56c-e8c436fb83e7-l2ueaq.mp4',
+  'https://utfs.io/f/c1379e93-113d-4b9e-979b-8bbdd6d4958e-z2yvdz.mp4',
+  'https://utfs.io/f/65be24a8-9221-4c3e-96db-306b3d7bba91-7kbevv.mp4',
+  'https://utfs.io/f/cef91fbb-2981-42fa-af89-285529c36266-me1m41.mp4',
+  'https://utfs.io/f/a5a7e474-dd4a-4e3d-96ae-a16688a32b15-xclw8l.mp4',
+  'https://utfs.io/f/e172cb16-b937-4286-b660-5ea766bc99cf-ikgowz.mp4',
+  'https://utfs.io/f/38df5d6a-2cfd-40f8-b4e1-f8b3b442386d-4z67u5.-(720p).mp4',
+  'https://utfs.io/f/c5e37a8f-2ae2-4911-84ed-ffe37afb17a0-8qjsy3.mp4',
+  'https://utfs.io/f/05143615-0cb4-48c0-b031-66a427e5f017-qm4u2t.mp4',
+]
