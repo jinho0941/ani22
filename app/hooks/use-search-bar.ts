390db09b68636d24c9e6a62fd2876e3283fdad42
daggax <daggax8235@gmail.com>
2024-08-07 02:01:13 +0900

    feat: search 에 필요한 hooks작성 한 파일에서 다 작성하니 복잡해서 분리함.

---
+++ app/hooks/use-search-bar.ts
@@ -0,0 +1,51 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { findEpisodeTitleByTitle } from '@/app/action/episode'
+import { useDebounce } from './use-debounce'
+
+const formSchema = z.object({
+  search: z.string().min(1).max(50),
+})
+
+export const useSearchBar = () => {
+  const router = useRouter()
+  const [results, setResults] = useState<string[]>([])
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: '',
+    },
+  })
+
+  const searchValue = form.watch('search')
+  const debouncedSearchValue = useDebounce(searchValue, 100)
+
+  const fetchEpisodes = async () => {
+    if (debouncedSearchValue) {
+      try {
+        const episodes = await findEpisodeTitleByTitle(debouncedSearchValue)
+        setResults(episodes)
+      } catch (error) {
+        console.error('Error searching episodes:', error)
+      }
+    } else {
+      setResults([])
+    }
+  }
+
+  useEffect(() => {
+    fetchEpisodes()
+  }, [debouncedSearchValue])
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const { search } = values
+    const query = new URLSearchParams({ find: search })
+    router.push(`/search?${query}`)
+  }
+
+  return { form, results, onSubmit }
+}
