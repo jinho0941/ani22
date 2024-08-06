8c57dc211da8cc877697f6df16bbcca413fe69ed
daggax <daggax8235@gmail.com>
2024-08-06 19:18:13 +0900

    feat: random string값 가져오는 함수 추가.

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,8 @@ export function cn(...inputs: ClassValue[]) {
 export const hashPassword = (password: string) => bcrypt.hashSync(password, 10)
 export const verifyPassword = (password: string, hashedPassword: string) =>
   bcrypt.compareSync(password, hashedPassword)
+
+export const getRandomString = (arr: string[]) => {
+  const randomIndex = Math.floor(Math.random() * arr.length)
+  return arr[randomIndex]
+}
