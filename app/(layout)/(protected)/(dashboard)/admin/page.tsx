d429019f84a2e591cf2608617fa6f2abea00b7b6
daggax <daggax8235@gmail.com>
2024-08-07 06:51:16 +0900

    feat: 관리자 페이지 작성완료, 관리자 페이지는 한페이지에 담기어려워서 라우팅 버튼만 넣어둠.

--- app/(layout)/(protected)/(dashboard)/admin/page.tsx
+++ app/(layout)/(protected)/(dashboard)/admin/page.tsx
@@ -1,5 +1,34 @@
+import { Book, User, Video } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
 const Page = () => {
-  return <div></div>
+  return (
+    <div className='grid md:grid-cols-2 grid-cols-1 gap-y-12'>
+      <div className='flex items-center gap-x-2'>
+        <User />
+        <span>유저 권한 관리 페이지</span>
+        <Button asChild>
+          <Link href={`/manage/user-request`}>유저 권한 요청</Link>
+        </Button>
+      </div>
+      <div className='flex items-center gap-x-2'>
+        <Book />
+        <span>에피소드 신청 관리 페이지</span>
+        <Button asChild>
+          <Link href={`/manage/episode-request`}>에피소드 신청</Link>
+        </Button>
+      </div>
+      <div className='flex items-center gap-x-2'>
+        <Video />
+        <span>비디오 신청 관리 페이지</span>
+        <Button asChild>
+          <Link href={`/manage/video-request`}>비디오 신청</Link>
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default Page
