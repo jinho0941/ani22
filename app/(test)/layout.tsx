213ea3c9abd1820a0905dba8d8d04947460cf0d0
daggax <daggax8235@gmail.com>
2024-08-08 04:49:15 +0900

    fix: 기존 데이터 생성 testpage는 삭제

--- app/(test)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='bg-slate-400 min-h-screen flex items-center justify-center'>
-      {children}
-    </div>
-  )
-}
-
-export default Layout
