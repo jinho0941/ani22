da64f74a81a4ab2cfab4b66eda8a1d8eb0156a6e
daggax <daggax8235@gmail.com>
2024-08-09 15:40:11 +0900

    fix: general mobile grid cols안넣어서 수정함.

--- app/(layout)/(dashboard)/general/page.tsx
+++ app/(layout)/(dashboard)/general/page.tsx
@@ -10,7 +10,7 @@ const Page = async () => {
       <h2 className='lg:text-2xl text-lg font-bold'>
         <span className='lg:text-xl text-sm font-medium'>pick</span>
         <span className='ml-1'>에피소드</span>
-        <div className='mt-6 grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6'>
+        <div className='mt-6 grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6'>
           <EpisodeList episodes={episodes} cursorId={cursorId} />
         </div>
       </h2>
